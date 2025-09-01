55f9e1d520f376e4def65cb167618b59013b27a3
xKuso69 <evan23weller@gmail.com>
2025-09-01 06:35:03 -0400

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,26 +1,31 @@
 package com.example.examplemod;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
-import net.fabricmc.fabric.api.event.player.PlayerDeathCallback;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 
 public class ExampleMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        // When any player dies
-        PlayerDeathCallback.EVENT.register((player, source) -> {
-            MinecraftServer server = player.getServer();
+        // Run after any LivingEntity dies
+        ServerLivingEntityEvents.AFTER_DEATH.register((LivingEntity entity, DamageSource source) -> {
+            // Only care if it's a player
+            if (!(entity instanceof ServerPlayerEntity victim)) return;
+
+            MinecraftServer server = victim.getServer();
             if (server == null) return;
 
-            String msg = "Weakest Link! Blame " + player.getName().getString() + "!";
+            // Broadcast blame message
+            String msg = "Weakest Link! Blame " + victim.getName().getString() + "!";
             server.getPlayerManager().broadcast(Text.literal(msg), false);
 
             // Kill all other players
             for (ServerPlayerEntity p : server.getPlayerManager().getPlayerList()) {
-                if (p == player) continue;
+                if (p == victim) continue;
                 p.kill();
             }
         });
